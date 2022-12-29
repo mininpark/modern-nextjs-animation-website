89bc8c9b13a7b433404a1a36ec4b4dd91e3c991c
mininpark <parkmini.93@gmail.com>
2022-12-29 22:32:16 +0100

    added hero section

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,40 +1,35 @@
 'use client';
 
-import { useRef } from 'react';
-// import { motion } from 'framer-motion';
+import { useRef, useEffect } from 'react';
 import { gsap } from 'gsap';
 import styles from '../styles';
-// import { navVariants } from '../utils/motion';
 
 const Navbar = () => {
-  const myRef = useRef();
+  const navbarRef = useRef();
 
-  const navbarRef = (element) => {
-    myRef.current = element;
-    gsap.fromTo(element, 1, { y: -40 }, { y: 0 });
-    console.log(myRef);
-  };
+  useEffect(() => {
+    gsap.fromTo(navbarRef.current, 1, { y: -100 }, { y: 0, delay: 0.5 });
+  }, []);
 
   return (
     <nav className={`${styles.xPaddings} py-8 relative`} ref={navbarRef}>
-      <div className="absoulte inset-0">
-        <div
-          className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
-        >
-          <img
-            src="/search.svg"
-            alt="search"
-            className="w-[24px] h-[24px] object-contain"
-          />
-          <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
-            METAVERSE
-          </h2>
-          <img
-            src="/menu.svg"
-            alt="menu"
-            className="w-[24px] h-[24px] object-contain"
-          />
-        </div>
+      <div className="absoulte inset-0 gradient-01" />
+      <div
+        className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+      >
+        <img
+          src="/search.svg"
+          alt="search"
+          className="w-[24px] h-[24px] object-contain"
+        />
+        <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
+          METAVERSE
+        </h2>
+        <img
+          src="/menu.svg"
+          alt="menu"
+          className="w-[24px] h-[24px] object-contain"
+        />
       </div>
     </nav>
   );
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,9 +1,74 @@
 'use client';
 
-const Hero = () => (
-  <section>
-    Hero section
-  </section>
-);
+import { useRef, useEffect } from 'react';
+
+import gsap from 'gsap';
+import styles from '../styles';
+
+const Hero = () => {
+  const heroRefTop = useRef();
+  const heroRefBottom = useRef();
+  const heroCover = useRef();
+
+  useEffect(() => {
+    gsap.fromTo(
+      heroRefTop.current,
+      { opacity: 0, y: 100 },
+      { opacity: 1, y: 0, duration: 0.5, delay: 0.5 },
+    );
+    gsap.fromTo(
+      heroRefBottom.current,
+      { opacity: 0, y: 100 },
+      { opacity: 1, y: 0, duration: 0.8, delay: 0.5 },
+    );
+    gsap.fromTo(
+      heroCover.current,
+      { opacity: 0, x: 100 },
+      { opacity: 1, x: 0, duration: 0.8 },
+    );
+  }, []);
+
+  return (
+    <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+      <div className={`${styles.innerWidth} mx-auto flex flex-col`}>
+        <div className="flex justify-center items-center flex-col relative z-10">
+          <h1 className={styles.heroHeading} ref={heroRefTop}>
+            Metaverse
+          </h1>
+          <div
+            className="flex flex-row justify-center items-center"
+            ref={heroRefBottom}
+          >
+            <h1 className={styles.heroHeading}>Ma</h1>
+            <div className={styles.heroDText}>D</div>
+            <h1 className={styles.heroHeading}>NESS</h1>
+          </div>
+        </div>
+        <div
+          className="relative w-full md:-mt-[20px] -mt-[12px]"
+          ref={heroCover}
+        >
+          {/* self-closing div element is used to create an element that is used for styling purposes only. */}
+          <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
+          <img
+            src="/cover.png"
+            alt="hero cover"
+            className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+          />
+
+          <a href="#explore">
+            <div className="w-full flex justify-end sm:mt-[70px] -mt-[50px] pr-[40px] relative z-10">
+              <img
+                src="/stamp.png"
+                alt="stamp"
+                className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
+              />
+            </div>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Hero;

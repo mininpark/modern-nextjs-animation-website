77ed654211832ce9b3671f0617c18360811130e3
mininpark <parkmini.93@gmail.com>
2023-01-15 14:54:01 +0100

    fixed text overflow out of ExploreCard

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -61,7 +61,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => {
     <div
       className={`${
         active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
-      } relative transition-[flex] duration-[2s] ease-in-out-flex cursor-pointer h-[700px] min-w-[170px] `}
+      } relative transition-[flex] duration-[1.8s] ease-in-out-flex cursor-pointer h-[700px] min-w-[170px] `}
       onClick={() => handleClick(id)}
     >
       <img
@@ -72,12 +72,12 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => {
       />
       {active !== id ? (
         <div className="absolute z-0 bottom-0 overflow-hidden h-full">
-          <h3 className="font-semibold text-white sm:text-[26px] pl-4 text:[18px] lg:rotate-[-90deg] translate-y-[-100px]">
+          <h3 className="font-semibold text-white sm:text-[26px] pl-4 text:[18px] lg:rotate-[-90deg] translate-y-[540px]">
             {title}
           </h3>
         </div>
       ) : (
-        <div className="absolute z-0 bottom-0 p-8 w-full bg-gray-2 ">
+        <div className="absolute z-0 bottom-0 p-8 w-full">
           <div
             className="w-[60px] h-[60px] glassmorphism rounded-[24px]"
             style={{ border: 'none' }}
@@ -91,7 +91,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => {
           <div className="text-white uppercase text-[16px] leading-[20px] my-5">
             Enter Metaverse
           </div>
-          <h3 className="font-semibold text-white sm:text-[26px] text:[18px]">
+          <h3 className="font-semibold text-white sm:text-[26px] text-[18px]">
             {title}
           </h3>
         </div>
